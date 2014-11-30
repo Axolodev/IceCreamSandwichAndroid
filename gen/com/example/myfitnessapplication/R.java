1bdfce0647bf00bf45ca0311c1c27a62e742bba8
Manuel Carrillo <itoi92@gmail.com>
2014-11-30 03:01:27 -0600

    Activity con resumen general agregado

--- gen/com/example/myfitnessapplication/R.java
+++ gen/com/example/myfitnessapplication/R.java
@@ -14,31 +14,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Calorias=0x7f060020;
-        public static final int Deporte=0x7f06001d;
-        public static final int Duracion=0x7f060026;
-        public static final int Fecha=0x7f060023;
-        public static final int TableLayout1=0x7f06001b;
-        public static final int TableRow01=0x7f06001f;
-        public static final int TableRow02=0x7f060022;
-        public static final int TableRow03=0x7f060025;
+        public static final int Calorias=0x7f060027;
+        public static final int Deporte=0x7f060024;
+        public static final int Duracion=0x7f06002d;
+        public static final int Fecha=0x7f06002a;
+        public static final int RelativeLayout1=0x7f06001a;
+        public static final int TableLayout1=0x7f060022;
+        public static final int TableRow01=0x7f060026;
+        public static final int TableRow02=0x7f060029;
+        public static final int TableRow03=0x7f06002c;
         public static final int button1=0x7f060003;
         public static final int button2=0x7f060002;
         public static final int button3=0x7f06000b;
         public static final int button4=0x7f060009;
         public static final int button5=0x7f06000a;
-        public static final int calorias_val=0x7f060021;
+        public static final int calorias_id2=0x7f06001b;
+        public static final int calorias_tag_id2=0x7f06001c;
+        public static final int calorias_val=0x7f060028;
         public static final int chronometer1=0x7f060008;
-        public static final int deporte_val=0x7f06001e;
-        public static final int duracion_val=0x7f060027;
+        public static final int deporte_val=0x7f060025;
+        public static final int duracion_val=0x7f06002e;
         public static final int editText1=0x7f060007;
         public static final int editText2=0x7f060006;
-        public static final int fecha_val=0x7f060024;
+        public static final int fecha_tag_id2=0x7f06001e;
+        public static final int fecha_val=0x7f06002b;
+        public static final int general_id2=0x7f06001d;
         public static final int imageButton1=0x7f060004;
         public static final int imageButton2=0x7f060005;
         public static final int imageView1=0x7f06000d;
         public static final int listView1=0x7f06000c;
-        public static final int numberPicker1=0x7f06001a;
+        public static final int numberPicker1=0x7f060021;
         public static final int radioButton1=0x7f06000f;
         public static final int radioButton2=0x7f060010;
         public static final int radioButton3=0x7f060011;
@@ -47,13 +52,15 @@ public final class R {
         public static final int radioButton6=0x7f060014;
         public static final int radioButton7=0x7f060015;
         public static final int radioDeporte=0x7f06000e;
-        public static final int tableRow1=0x7f06001c;
+        public static final int tableRow1=0x7f060023;
         public static final int textView1=0x7f060000;
         public static final int textView2=0x7f060001;
         public static final int textView3=0x7f060016;
         public static final int textView4=0x7f060017;
         public static final int textView5=0x7f060018;
         public static final int textView6=0x7f060019;
+        public static final int tiempo_id2=0x7f06001f;
+        public static final int tiempo_tag_id2=0x7f060020;
     }
     public static final class layout {
         public static final int activity_configuracion=0x7f030000;
@@ -63,22 +70,27 @@ public final class R {
         public static final int activity_medicion=0x7f030004;
         public static final int activity_playlist=0x7f030005;
         public static final int activity_resultado=0x7f030006;
-        public static final int activity_videos=0x7f030007;
-        public static final int row=0x7f030008;
+        public static final int activity_resumen=0x7f030007;
+        public static final int activity_videos=0x7f030008;
+        public static final int row=0x7f030009;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int calorias_quemadas=0x7f040010;
         public static final int calorias_tag=0x7f04000c;
         public static final int deporte_tag=0x7f04000a;
         public static final int duracion_tag=0x7f040009;
         public static final int fecha_tag=0x7f04000b;
+        public static final int general=0x7f04000e;
         public static final int hello_world=0x7f040001;
+        public static final int tiempo=0x7f04000f;
         public static final int title_activity_configuracion=0x7f040003;
         public static final int title_activity_cronometro=0x7f040005;
         public static final int title_activity_historial=0x7f040004;
         public static final int title_activity_medicion=0x7f040006;
         public static final int title_activity_playlist=0x7f040007;
         public static final int title_activity_resultado=0x7f040008;
+        public static final int title_activity_resumen=0x7f04000d;
         public static final int title_activity_videos=0x7f040002;
     }
     public static final class style {
---
+++ src/com/example/myfitnessapplication/ResumenActivity.java
@@ -0,0 +1,36 @@
+package com.example.myfitnessapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ResumenActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_resumen);
+		TextView calorias = (TextView) findViewById(R.id.calorias_id2);
+		TextView duracion = (TextView) findViewById(R.id.tiempo_id2);
+		TextView fecha = (TextView) findViewById(R.id.fecha_tag_id2);
+		DeporteOperations dao;
+		dao = new DeporteOperations(this);
+		dao.open();
+		Deporte equipo = dao.findEquipo(1);
+		double caloriasSum = equipo.getCalorias();
+		double duracionSum = equipo.getTiempo();
+		fecha.setText("Inicio: " + equipo.getFecha());
+		for (int i = 2; i < dao.numRows() + 1; i++) {
+			equipo = dao.findEquipo(i);
+			if (equipo != null) {
+				caloriasSum += equipo.getCalorias();
+				duracionSum += equipo.getTiempo();
+			}
+
+		}
+		duracion.setText(String.valueOf(duracionSum));
+		calorias.setText(String.valueOf(caloriasSum));
+	}
+}
