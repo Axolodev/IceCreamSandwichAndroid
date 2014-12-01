575d0b2f4b45a98345436d0555d7655c56efd5fb
mortaldraw <robildruirey@hotmail.com>
2014-12-01 13:04:00 -0600

    Strings dessincronizado

--- src/com/example/myfitnessapplication/Videos.java
+++ src/com/example/myfitnessapplication/Videos.java
@@ -1,9 +1,8 @@
 package com.example.myfitnessapplication;
 
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class Videos extends Activity {
 
