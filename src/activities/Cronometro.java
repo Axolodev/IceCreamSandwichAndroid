3829aaad0b6fd1d403aa11a81ada713a85b868e9
mortaldraw <robildruirey@hotmail.com>
2014-12-01 21:19:42 -0600

    Facebook arreglado

--- src/activities/Cronometro.java
+++ src/activities/Cronometro.java
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
-import android.widget.Toast;
 
 import com.example.myfitnessapplication.Deporte;
 import com.example.myfitnessapplication.DeporteOperations;
