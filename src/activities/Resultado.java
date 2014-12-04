eaf37d588678a356594233a9349c52907407ba1b
mortaldraw <robildruirey@hotmail.com>
2014-12-03 19:55:43 -0600

    Formateo, isOnline() y compartir

--- src/activities/Resultado.java
+++ src/activities/Resultado.java
@@ -26,18 +26,16 @@ import com.facebook.widget.FacebookDialog;
 public class Resultado extends Activity {
 
 	String deporteS;
-	Long tiempoS,tiempomin,tiempohr;
-        Button buMenu;
-	//private Double calori;
-	//Button menu;
-	Long calori=(long) 1;
-	TextView calorias,deporte,tiempo;//3 tiempo,4 calorias,6 deporte
+	Long tiempoS, tiempomin, tiempohr;
+	Button buMenu;
+	// private Double calori;
+	// Button menu;
+	Long calori = (long) 1;
+	TextView calorias, deporte, tiempo;// 3 tiempo,4 calorias,6 deporte
 	DeporteOperations dao;
-	
 
-
-	//TextView tvCalorias, tvDeporte, tvTiempo;// 3 tiempo,4 calorias,6
-														// deporte
+	// TextView tvCalorias, tvDeporte, tvTiempo;// 3 tiempo,4 calorias,6
+	// deporte
 	private UiLifecycleHelper uiHelper; // Usado para compartir datos en
 										// Facebook
 
@@ -50,104 +48,103 @@ public class Resultado extends Activity {
 		buMenu = (Button) findViewById(R.id.button1);
 		dao = new DeporteOperations(this);
 		dao.open();
-		/*tvCalorias = (TextView) findViewById(R.id.textView4);
-		tvDeporte = (TextView) findViewById(R.id.textView6);
-		tvTiempo = (TextView) findViewById(R.id.textView3);
+		/*
+		 * tvCalorias = (TextView) findViewById(R.id.textView4); tvDeporte =
+		 * (TextView) findViewById(R.id.textView6); tvTiempo = (TextView)
+		 * findViewById(R.id.textView3); buCompartirFace = (ImageButton)
+		 * findViewById(R.id.buShareFacebook);
+		 * 
+		 * Bundle datos = getIntent().getExtras(); if (datos != null) { deporteS
+		 * = datos.getString("deporte"); tiempoS = datos.getLong("tiempo");
+		 * double tempCalorias = datos.getDouble("calorias");
+		 * 
+		 * tvCalorias.setText(String.valueOf(tempCalorias));
+		 * 
+		 * tvDeporte.setText(deporteS); String tiempos =
+		 * String.valueOf(tiempoS); tvTiempo.setText(tiempos); }
+		 */
 		buCompartirFace = (ImageButton) findViewById(R.id.buShareFacebook);
+		calorias = (TextView) findViewById(R.id.textView4);
+		deporte = (TextView) findViewById(R.id.textView6);
+		String deporteF;
+		tiempo = (TextView) findViewById(R.id.textView3);
+		StringBuffer stringBuffer = new StringBuffer();
+		try {
+			// Attaching BufferedReader to the FileInputStream by the help of
+			// InputStreamReader
+			BufferedReader inputReader = new BufferedReader(
+					new InputStreamReader(openFileInput("deporteSel")));
+			String inputString;
+			// Reading data line by line and storing it into the stringbuffer
+			while ((inputString = inputReader.readLine()) != null) {
+				stringBuffer.append(inputString + "\n");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		deporteF = stringBuffer.toString();
 
 		Bundle datos = getIntent().getExtras();
 		if (datos != null) {
 			deporteS = datos.getString("deporte");
-			tiempoS = datos.getLong("tiempo");
-			double tempCalorias = datos.getDouble("calorias");
+			tiempoS = datos.getLong("segundos");
+			tiempomin = datos.getLong("minutos");
+			tiempohr = datos.getLong("horas");
 
-			tvCalorias.setText(String.valueOf(tempCalorias));
-
-			tvDeporte.setText(deporteS);
 			String tiempos = String.valueOf(tiempoS);
-			tvTiempo.setText(tiempos);
-		}*/
-		buCompartirFace = (ImageButton) findViewById(R.id.buShareFacebook);
-calorias=(TextView)findViewById(R.id.textView4);
-		deporte=(TextView)findViewById(R.id.textView6);
-		String deporteF;
-		tiempo=(TextView)findViewById(R.id.textView3);
-		StringBuffer stringBuffer = new StringBuffer();    
-        try {  
-            //Attaching BufferedReader to the FileInputStream by the help of InputStreamReader  
-            BufferedReader inputReader = new BufferedReader(new InputStreamReader(  
-                    openFileInput("deporteSel")));  
-            String inputString;  
-            //Reading data line by line and storing it into the stringbuffer                
-            while ((inputString = inputReader.readLine()) != null) {  
-                stringBuffer.append(inputString + "\n");  
-            }  
-              
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        }  
-        deporteF=stringBuffer.toString();
-		
-		Bundle datos=getIntent().getExtras();
-		if (datos!=null){
-			deporteS=datos.getString("deporte");
-			tiempoS=datos.getLong("segundos");
-			tiempomin=datos.getLong("minutos");
-			tiempohr=datos.getLong("horas");
-			
-			String tiempos=String.valueOf(tiempoS);
-			String hrs=String.valueOf(tiempohr);
-			String min=String.valueOf(tiempomin);
-			tiempo.setText(hrs+":"+min+":"+tiempos); 
+			String hrs = String.valueOf(tiempohr);
+			String min = String.valueOf(tiempomin);
+			tiempo.setText(hrs + ":" + min + ":" + tiempos);
 		}
 		deporte.setText(deporteF);
-		Toast.makeText(getApplicationContext(), "deporte:" + deporteF, Toast.LENGTH_LONG).show();
-	
-	if (deporteF.equals("Spinning")){
-		
-		calori=(long) ((70 * 2.2) * (tiempomin+(tiempohr*60)) * .053);
-		Toast.makeText(getApplicationContext(), "tiempo:" + calori, Toast.LENGTH_LONG).show();
-	}
-	else if(deporteF.equals("Caminar")){
-		Toast.makeText(getApplicationContext(), "tiempo1:" + calori, Toast.LENGTH_LONG).show();
-		calori=(long) ((70 * 2.2) * (tiempomin+(tiempohr*60))* .062);
-		Toast.makeText(getApplicationContext(), "tiempo:" + calori, Toast.LENGTH_LONG).show();
-		
-	}
-	else if (deporteF.equals("Baloncesto")){
-		calori=(long) ((70 * 2.2) * (tiempomin+(tiempohr*60)) * .045);
-		Toast.makeText(getApplicationContext(), "tiempo:" + calori, Toast.LENGTH_LONG).show();
-		
-	}
-	else if(deporteF.equals("Futbol")){
-		calori=(long) ((70 * 2.2) * (tiempomin+(tiempohr*60))* .061);
-		Toast.makeText(getApplicationContext(), "tiempo:" + calori, Toast.LENGTH_LONG).show();
-		
-	}
-	
-	else{
-		calori=(long) ((70 * 2.2) * (tiempomin+(tiempohr*60))* .142);
-	}
-		String caloriasF=String.valueOf(calori);
+		Toast.makeText(getApplicationContext(), "deporte:" + deporteF,
+				Toast.LENGTH_LONG).show();
+
+		if (deporteF.equals("Spinning")) {
+
+			calori = (long) ((70 * 2.2) * (tiempomin + (tiempohr * 60)) * .053);
+			Toast.makeText(getApplicationContext(), "tiempo:" + calori,
+					Toast.LENGTH_LONG).show();
+		} else if (deporteF.equals("Caminar")) {
+			Toast.makeText(getApplicationContext(), "tiempo1:" + calori,
+					Toast.LENGTH_LONG).show();
+			calori = (long) ((70 * 2.2) * (tiempomin + (tiempohr * 60)) * .062);
+			Toast.makeText(getApplicationContext(), "tiempo:" + calori,
+					Toast.LENGTH_LONG).show();
+
+		} else if (deporteF.equals("Baloncesto")) {
+			calori = (long) ((70 * 2.2) * (tiempomin + (tiempohr * 60)) * .045);
+			Toast.makeText(getApplicationContext(), "tiempo:" + calori,
+					Toast.LENGTH_LONG).show();
+
+		} else if (deporteF.equals("Futbol")) {
+			calori = (long) ((70 * 2.2) * (tiempomin + (tiempohr * 60)) * .061);
+			Toast.makeText(getApplicationContext(), "tiempo:" + calori,
+					Toast.LENGTH_LONG).show();
+
+		}
+
+		else {
+			calori = (long) ((70 * 2.2) * (tiempomin + (tiempohr * 60)) * .142);
+		}
+		String caloriasF = String.valueOf(calori);
 		calorias.setText(caloriasF);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 		String date = sdf.format(new Date());
 
-		Deporte deporte = new Deporte(deporteF, date, calori, tiempoS+tiempomin*60+tiempohr*3600);
+		Deporte deporte = new Deporte(deporteF, date, calori, tiempoS
+				+ tiempomin * 60 + tiempohr * 3600);
 		dao.addDeporte(deporte);
-		/*menu.setOnClickListener(new OnClickListener(){
-			public void onClick(View v){
-				Intent medi=new Intent(Resultado.this, MainActivity.class);
-				medi.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				medi.putExtra("deporte", deporteS);
-				medi.putExtra("calorias", calori);
-				medi.putExtra("tiempo", tiempoS);
-				
-				startActivity(medi);
-			}
-		});*/
-
-
+		/*
+		 * menu.setOnClickListener(new OnClickListener(){ public void
+		 * onClick(View v){ Intent medi=new Intent(Resultado.this,
+		 * MainActivity.class); medi.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		 * medi.putExtra("deporte", deporteS); medi.putExtra("calorias",
+		 * calori); medi.putExtra("tiempo", tiempoS);
+		 * 
+		 * startActivity(medi); } });
+		 */
 
 		uiHelper = new UiLifecycleHelper(this, null);
 		uiHelper.onCreate(savedInstanceState);
@@ -164,16 +161,13 @@ calorias=(TextView)findViewById(R.id.textView4);
 		buMenu.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 
-
-				Intent medi=new Intent(Resultado.this, MainActivity.class);
+				Intent medi = new Intent(Resultado.this, MainActivity.class);
 				medi.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				medi.putExtra("deporte", deporteS);
 				medi.putExtra("calorias", calori);
 				medi.putExtra("tiempo", tiempoS);
-				
-				startActivity(medi);
 
-				
+				startActivity(medi);
 
 				finish();
 			}
@@ -187,10 +181,13 @@ calorias=(TextView)findViewById(R.id.textView4);
 
 				FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(
 						Resultado.this)
-						.setLink("https://developers.facebook.com/android")
+						.setLink(
+								"https://github.com/mortaldraw/IceCreamSandwichAndroid")
 						.setDescription(
-								"¡Acabo de hacer " + time + " de ejercicio!")
-						.build();
+								"Acabo de hacer " + tiempohr + " horas con "
+										+ tiempomin + " minutos y " + tiempoS
+										+ " segundos de ejercicio!")
+						.setApplicationName("MyFitnessApp").build();
 				uiHelper.trackPendingDialogCall(shareDialog.present());
 
 			}
